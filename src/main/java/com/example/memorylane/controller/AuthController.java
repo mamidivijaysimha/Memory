b33677509b789a76a5128a2097c851b4ac6d77cb
mamidivijaysimha <163118995+mamidivijaysimha@users.noreply.github.com>
2025-07-08 14:36:27 +0530

    Update AuthController.java

--- src/main/java/com/example/memorylane/controller/AuthController.java
+++ src/main/java/com/example/memorylane/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "https://silver-crepe-177b8f.netlify.app", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
